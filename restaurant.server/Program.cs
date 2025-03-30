ca29fded317b4c28e60ffe8f2e955fe340e64de9
Alexey Shatskikh <shtskkh@gmail.com>
2025-03-30 13:00:21 +0700

    Добавлен PositionsRepository

--- restaurant.server/Program.cs
+++ restaurant.server/Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using restaurant.server.Context;
+using restaurant.server.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContextPool<RestaurantContext>(opt => 
     opt.UseNpgsql(builder.Configuration.GetConnectionString("RestaurantContext")));
+
+builder.Services.AddScoped<IPositionsRepository, PositionsRepository>();
 
 var app = builder.Build();
 
---
+++ restaurant.server/Repositories/PositionsRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using restaurant.server.Context;
+using restaurant.server.Models;
+
+namespace restaurant.server.Repositories;
+
+public interface IPositionsRepository
+{
+    Task<List<Position>> GetAll(); 
+    Task<Position?> GetById(int id);
+    Task<Position?> GetByTitle(string title);
+    Task Add(Position position);
+    Task Update(Position position);
+    Task Delete(int id);
+}
+
+public class PositionsRepository(RestaurantContext context) : IPositionsRepository
+{
+    public async Task<List<Position>> GetAll()
+    {
+        return await context.Positions.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Position?> GetById(int id)
+    {
+        return await context.Positions.AsNoTracking().FirstOrDefaultAsync(p => p.IdPosition == id);
+    }
+
+    public async Task<Position?> GetByTitle(string title)
+    {
+        return await context.Positions.AsNoTracking().FirstOrDefaultAsync(p => p.Title == title);
+    }
+
+    public async Task Add(Position position)
+    {
+        context.Positions.Add(position);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task Update(Position position)
+    {
+        context.Entry(position).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+    }
+
+    public async Task Delete(int id)
+    {
+        var position = await context.Positions.FirstOrDefaultAsync(p => p.IdPosition == id);
+        if (position != null)
+        {
+            context.Positions.Remove(position);
+            await context.SaveChangesAsync();
+        }
+    }
+}
