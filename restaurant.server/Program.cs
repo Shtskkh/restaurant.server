78e4e77c5b49f348d401080b7ab7b885a1b5c4f6
Alexey Shatskikh <shtskkh@gmail.com>
2025-03-30 14:25:45 +0700

    Добавлена аутентификация с использованием JWT токенов

--- restaurant.server/Program.cs
+++ restaurant.server/Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using restaurant.server.Context;
 using restaurant.server.Repositories;
 
@@ -8,9 +11,28 @@ builder.Services.AddDbContextPool<RestaurantContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("RestaurantContext"), 
         o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!))
+        };
+    });
+builder.Services.AddAuthorization();
+
 builder.Services.AddScoped<IPositionsRepository, PositionsRepository>();
 builder.Services.AddScoped<IStaffRepository, StaffRepository>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
