f53c1939e324020c56422c6ddcebd307eeb7dea7
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-09 12:39:36 +0700

    Добавлен контроллер для Orders

---
+++ restaurant.server/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restaurant.server.Services;
+
+namespace restaurant.server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrdersController(IOrdersService ordersService) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await ordersService.GetAll());
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var order = await ordersService.GetById(id);
+        if (order == null)
+            return NotFound();
+        return Ok(order);
+    }
+}
