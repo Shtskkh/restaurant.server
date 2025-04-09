a063d4302a112a37c4e818113e702a919e9db5c3
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-09 12:34:11 +0700

    Добавлен OrderModel

---
+++ restaurant.server/DTOs/OrderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace restaurant.server.DTOs;
+
+public class OrderModel
+{
+    public int IdOrder { get; set; }
+    public DateTime Date { get; set; }
+    public int TableNumber { get; set; }
+    public string Status { get; set; }
+    public string? Employee { get; set; }
+}
