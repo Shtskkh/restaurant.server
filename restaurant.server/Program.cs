745e2b469e77c6f4085c12a0e2fe4c98104ce970
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-16 16:08:34 +0700

    Добавлен Swagger

--- restaurant.server/Program.cs
+++ restaurant.server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using restaurant.server.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,22 @@ builder.Services.AddDbContextPool<RestaurantContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("RestaurantContext"),
         o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ASP.NET API ресторана"
+    });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
