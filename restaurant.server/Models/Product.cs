8ed098be929732761f6ada8c8e77cc6aa62800b2
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-16 15:52:53 +0700

    Добавлен контекст базы данных

---
+++ restaurant.server/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace restaurant.server.Models;
+
+[Table("products")]
+public partial class Product
+{
+    [Key]
+    [Column("id_product")]
+    public int IdProduct { get; set; }
+
+    [Column("title")]
+    public string Title { get; set; } = null!;
+
+    [InverseProperty("IdProductNavigation")]
+    public virtual ICollection<ProductsInDish> ProductsInDishes { get; set; } = new List<ProductsInDish>();
+
+    [InverseProperty("IdProductNavigation")]
+    public virtual ICollection<Supply> Supplies { get; set; } = new List<Supply>();
+}
---
+++ restaurant.server/Models/ProductsInDish.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace restaurant.server.Models;
+
+[PrimaryKey("IdDish", "IdProduct")]
+[Table("products_in_dish")]
+public partial class ProductsInDish
+{
+    [Key]
+    [Column("id_dish")]
+    public int IdDish { get; set; }
+
+    [Key]
+    [Column("id_product")]
+    public int IdProduct { get; set; }
+
+    [Column("count")]
+    public decimal Count { get; set; }
+
+    [Column("id_unit")]
+    public int IdUnit { get; set; }
+
+    [ForeignKey("IdDish")]
+    [InverseProperty("ProductsInDishes")]
+    public virtual Dish IdDishNavigation { get; set; } = null!;
+
+    [ForeignKey("IdProduct")]
+    [InverseProperty("ProductsInDishes")]
+    public virtual Product IdProductNavigation { get; set; } = null!;
+
+    [ForeignKey("IdUnit")]
+    [InverseProperty("ProductsInDishes")]
+    public virtual MeasureUnit IdUnitNavigation { get; set; } = null!;
+}
