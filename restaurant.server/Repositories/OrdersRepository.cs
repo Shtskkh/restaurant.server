6a663073f5ec2f16011fb8456699881598d0bf6c
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-17 11:59:18 +0700

    Refactoring: улучшена логика получения информации о заказе

--- restaurant.server/Repositories/OrdersRepository.cs
+++ restaurant.server/Repositories/OrdersRepository.cs
@@ -1,43 +1,84 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections;
+using Microsoft.EntityFrameworkCore;
 using restaurant.server.Context;
+using restaurant.server.DTOs;
 using restaurant.server.Models;
 
 namespace restaurant.server.Repositories;
 
 public interface IOrdersRepository
 {
-    Task<IEnumerable<Order>> GetAll();
-    Task<Order?> GetById(int id);
-    Task<IEnumerable<DishesInOrder>> GetDishesInOrder(int id);
+    Task<List<OrderModel>> GetAll();
+    Task<OrderModel?> GetById(int id);
+    Task<List<DishInOrderModel>> GetDishesInOrder(int id);
 }
 
 public class OrdersRepository(RestaurantContext context) : IOrdersRepository
 {
-    public async Task<IEnumerable<Order>> GetAll()
+    public async Task<List<OrderModel>> GetAll()
     {
-        return await context.Orders.AsNoTracking()
-            .Include(o => o.IdEmployeeNavigation)
-            .Include(o => o.IdStatusNavigation)
-            .Include(o => o.IdTableNavigation)
-            .ToListAsync();
+        var orders =
+            from order in context.Orders.AsNoTracking()
+            join table in context.Tables.AsNoTracking()
+                on order.IdTable equals table.IdTable
+            join status in context.Statuses.AsNoTracking()
+                on order.IdStatus equals status.IdStatus
+            join employee in context.Staff.AsNoTracking()
+                on order.IdEmployee equals employee.IdEmployee
+            select new OrderModel
+            {
+                IdOrder = order.IdOrder,
+                Date = order.Date,
+                TableNumber = table.Number,
+                Status = status.Title,
+                Employee = $"{employee.LastName} {employee.FirstName} {employee.MiddleName}"
+            };
+        
+        return await orders.ToListAsync();
     }
 
-    public async Task<Order?> GetById(int id)
+    public async Task<OrderModel?> GetById(int id)
     {
-        return await context.Orders
-            .AsNoTracking()
-            .Include(o => o.IdEmployeeNavigation)
-            .Include(o => o.IdStatusNavigation)
-            .Include(o => o.IdTableNavigation)
-            .FirstOrDefaultAsync(o => o.IdOrder == id);
+        var orders =
+            from order in context.Orders.AsNoTracking()
+            where order.IdOrder == id
+            join table in context.Tables.AsNoTracking()
+                on order.IdTable equals table.IdTable
+            join status in context.Statuses.AsNoTracking()
+                on order.IdStatus equals status.IdStatus
+            join employee in context.Staff.AsNoTracking()
+                on order.IdEmployee equals employee.IdEmployee
+            select new OrderModel
+            { 
+                IdOrder = order.IdOrder,
+                Date = order.Date,
+                TableNumber = table.Number,
+                Status = status.Title,
+                Employee = $"{employee.LastName} {employee.FirstName} {employee.MiddleName}"
+            };
+        
+        return await orders.FirstOrDefaultAsync();
     }
-    public async Task<IEnumerable<DishesInOrder>> GetDishesInOrder(int id)
+
+    public async Task<List<DishInOrderModel>> GetDishesInOrder(int id)
     {
-        return await context.DishesInOrders
-            .AsNoTracking()
-            .Where(o => o.IdOrder == id)
-            .Include(d => d.IdStatusNavigation)
-            .Include(d => d.IdDishNavigation)
-            .ToListAsync();
+        var dishes =
+            from orderDish in context.DishesInOrders.AsNoTracking()
+            where orderDish.IdOrder == id
+            join dish in context.Dishes.AsNoTracking()
+                on orderDish.IdDish equals dish.IdDish
+            join status in context.Statuses.AsNoTracking()
+                on orderDish.IdStatus equals status.IdStatus
+            select new DishInOrderModel
+            {
+                IdDish = dish.IdDish,
+                Title = dish.Title,
+                Count = orderDish.Count,
+                Comment = orderDish.Comment,
+                Status = status.Title,
+                TotalCost = orderDish.Count * dish.Cost
+            };
+        
+        return await dishes.ToListAsync();
     }
 }
--- restaurant.server/Services/OrdersService.cs
+++ restaurant.server/Services/OrdersService.cs
@@ -9,25 +9,13 @@ public interface IOrderService
 {
     Task<List<OrderModel>> GetAll();
     Task<OrderModel?> GetById(int id);
-    Task<List<DishInOrderModel>> GetDishesInOrder(int id);
 }
 
 public class OrdersService(IOrdersRepository ordersRepository) : IOrderService
 {
     public async Task<List<OrderModel>> GetAll()
     {
-        var orders = await ordersRepository.GetAll();
-        var orderModels = orders.Select(o => new OrderModel
-        {
-            IdOrder = o.IdOrder,
-            Date = o.Date,
-            TableNumber = o.IdTableNavigation.Number,
-            Status = o.IdStatusNavigation.Title,
-            Employee =
-                $"{o.IdEmployeeNavigation.LastName} {o.IdEmployeeNavigation.FirstName} {o.IdEmployeeNavigation.MiddleName}"
-        });
-
-        return orderModels.ToList();
+        return await ordersRepository.GetAll();
     }
 
     public async Task<OrderModel?> GetById(int id)
@@ -36,35 +24,9 @@ public class OrdersService(IOrdersRepository ordersRepository) : IOrderService
         if (order == null)
             return null;
         
-        var dishesInOrder = await GetDishesInOrder(id);
-        var orderModel = new OrderModel
-        {
-            IdOrder = order.IdOrder,
-            Date = order.Date,
-            TableNumber = order.IdTableNavigation.Number,
-            Status = order.IdStatusNavigation.Title,
-            Employee =
-                $"{order.IdEmployeeNavigation.LastName} {order.IdEmployeeNavigation.FirstName} {order.IdEmployeeNavigation.MiddleName}",
-            Dishes = dishesInOrder
-        };
-
-        return orderModel;
-    }
-
-    public async Task<List<DishInOrderModel>> GetDishesInOrder(int id)
-    {
-        var dishesInOrder = await ordersRepository.GetDishesInOrder(id);
+        var dishes = await ordersRepository.GetDishesInOrder(id);
+        order.Dishes = dishes;
         
-        var dishesInOrderModels = dishesInOrder.Select(d => new DishInOrderModel
-        {
-            IdDish = d.IdDish,
-            Title = d.IdDishNavigation.Title,
-            Count = d.Count,
-            Comment = d.Comment,
-            Status = d.IdStatusNavigation.Title,
-            TotalCost = d.Count * d.IdDishNavigation.Cost
-        });
-        
-        return dishesInOrderModels.ToList();
+        return order;
     }
 }
