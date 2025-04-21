c44581b5b4226b8dfe20b20b1dead9ca042ae0a0
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 11:00:00 +0700

    DI: StaffRepository

--- restaurant.server/Program.cs
+++ restaurant.server/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using restaurant.server.Context;
+using restaurant.server.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContextPool<RestaurantContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("RestaurantContext"),
         o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+
+builder.Services.AddScoped<IStaffRepository, StaffRepository>();
 
 var app = builder.Build();
 
