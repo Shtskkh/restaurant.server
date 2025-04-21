028bec1a1e36a87d7f10ca8b4153e3053d0e92a6
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 12:01:53 +0700

    DTOs: OrderModel, DishInOrderModel

---
+++ restaurant.server/Controllers/DishInOrderModel.cs
@@ -0,0 +1,11 @@
+﻿namespace restaurant.server.Controllers;
+
+public class DishInOrderModel
+{
+    public required int IdDish { get; set; }
+    public required string Title { get; set; }
+    public required int Count { get; set; }
+    public string? Comment { get; set; }
+    public required string Status { get; set; }
+    public required decimal TotalCost { get; set; }
+}
---
+++ restaurant.server/Controllers/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿namespace restaurant.server.Controllers;
+
+public class OrderModel
+{
+    public required int IdOrder { get; set; }
+    public required DateTime Date { get; set; }
+    public required int TableNumber { get; set; }
+    public required string Status { get; set; }
+    public required string Employee { get; set; }
+    public List<DishInOrderModel>? DishesInOrder { get; set; }
+}
