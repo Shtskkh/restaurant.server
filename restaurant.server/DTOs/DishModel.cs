2305ce8b3723fcada5fdaf7d7a275dc93e59d6f4
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 12:38:07 +0700

    DTOs: добавлен DishModel

---
+++ restaurant.server/DTOs/DishModel.cs
@@ -0,0 +1,11 @@
+﻿namespace restaurant.server.DTOs;
+
+public class DishModel
+{
+    public required int IdDish { get; set; }
+    public required string Title { get; set; }
+    public required decimal Cost { get; set; }
+    public required bool Availability { get; set; }
+    public required decimal WeightVolume { get; set; }
+    public required string Unit { get; set; }
+}
