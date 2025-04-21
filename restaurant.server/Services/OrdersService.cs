2d3573cff2619f5b8b16bdbbcf6e21cf05d9d54d
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 12:09:08 +0700

    OrdersService: GetAll, GetById

---
+++ restaurant.server/Services/OrdersService.cs
@@ -0,0 +1,31 @@
+﻿using restaurant.server.Controllers;
+using restaurant.server.Repositories;
+
+namespace restaurant.server.Services;
+
+public interface IOrdersService
+{
+    Task<List<OrderModel>> GetAll();
+    Task<OrderModel?> GetById(int id);
+}
+
+public class OrdersService(IOrdersRepository ordersRepository) : IOrdersService
+{
+    public async Task<List<OrderModel>> GetAll()
+    {
+        return await ordersRepository.GetAll();
+    }
+
+    public async Task<OrderModel?> GetById(int id)
+    {
+        var order = await ordersRepository.GetById(id);
+
+        if (order == null)
+            return null;
+
+        var dishes = await ordersRepository.GetDishesInOrder(order.IdOrder);
+
+        order.DishesInOrder = dishes;
+        return order;
+    }
+}
