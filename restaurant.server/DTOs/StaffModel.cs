c3bb81b3fcbb4a0b3d6e2ce1f6a1a0e67b25b0e1
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 10:57:11 +0700

    Добавлен StaffModel

---
+++ restaurant.server/DTOs/StaffModel.cs
@@ -0,0 +1,11 @@
+﻿namespace restaurant.server.DTOs;
+
+public class StaffModel
+{
+    public required int IdEmployee { get; set; }
+    public required string Position { get; set; }
+    public required string LastName { get; set; }
+    public required string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public required string PhoneNumber { get; set; }
+}
