b70ee50686058cdce7b27fca3d4d946d5554581b
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 11:37:10 +0700

    StaffController: дополнительные проверки, документирование статус кодов

--- restaurant.server/Controllers/StaffController.cs
+++ restaurant.server/Controllers/StaffController.cs
@@ -11,17 +11,23 @@ public class StaffController(IStaffService staffService) : Controller
 {
     [HttpGet("GetAll")]
     [ProducesResponseType(typeof(List<StaffModel>), 200)]
+    [ProducesResponseType(204)]
     public async Task<IActionResult> GetAll()
     {
+        var staff = await staffService.GetAll();
+        if (staff.Count == 0)
+            return NoContent();
+        
         return Ok(await staffService.GetAll());
     }
     
-    [HttpGet("GetById")]
+    [HttpGet("GetById/{id:int}")]
     [ProducesResponseType(typeof(StaffModel), 200)]
+    [ProducesResponseType(400)]
     [ProducesResponseType(404)]
     public async Task<IActionResult> GetById(int id)
     {
-        if (id <= 1)
+        if (id < 1)
             return BadRequest();
         
         var staff = await staffService.GetById(id);
