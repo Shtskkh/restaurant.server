b2012a66b8bbf9acbfb43cd966fa82160f11ef4e
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 12:47:04 +0700

    DTOs: ProductInDishModel

---
+++ restaurant.server/DTOs/ProductInDishModel.cs
@@ -0,0 +1,9 @@
+﻿namespace restaurant.server.DTOs;
+
+public class ProductInDishModel
+{
+    public required int IdProduct { get; set; }
+    public required string Title { get; set; }
+    public required decimal Count { get; set; }
+    public required string Unit { get; set; }
+}
