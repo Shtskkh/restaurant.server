94d0d3836d02652dc9da4287ef631767d00e9897
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 11:01:04 +0700

    Добавлен Swagger

--- restaurant.server/Program.cs
+++ restaurant.server/Program.cs
@@ -10,6 +10,15 @@ builder.Services.AddDbContextPool<RestaurantContext>(opt =>
 
 builder.Services.AddScoped<IStaffRepository, StaffRepository>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
