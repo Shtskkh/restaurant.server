aec5f17268aaaa9ae59720d9f6b400fc2b864a67
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 12:21:53 +0700

    OrdersController: GetAll, GetById

---
+++ restaurant.server/Controllers/OrdersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restaurant.server.DTOs;
+using restaurant.server.Services;
+
+namespace restaurant.server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class OrdersController(IOrdersService ordersService) : Controller
+{
+    [HttpGet("GetAll")]
+    [ProducesResponseType(typeof(List<OrderModel>), 200)]
+    [ProducesResponseType(204)]
+    public async Task<IActionResult> GetAll()
+    {
+        var orders = await ordersService.GetAll();
+        if (orders.Count == 0)
+            return NoContent();
+
+        return Ok(orders);
+    }
+
+    [HttpGet("GetById/{id:int}")]
+    [ProducesResponseType(typeof(OrderModel), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetById(int id)
+    {
+        if (id < 1)
+            return BadRequest(new ProblemDetails
+            {
+                Title = "ID должен быть больше 0.",
+                Status = 400
+            });
+
+        var order = await ordersService.GetById(id);
+        if (order == null)
+            return NotFound(new ProblemDetails
+            {
+                Title = "Заказ с таким ID не найден.",
+                Status = 404
+            });
+
+        return Ok(order);
+    }
+}
