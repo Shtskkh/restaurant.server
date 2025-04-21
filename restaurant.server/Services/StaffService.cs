3d6bc223c094b1f4955c7e702f97130ab01ffc19
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 11:09:25 +0700

    StaffService: добавлены GetAll, GetById

---
+++ restaurant.server/Services/StaffService.cs
@@ -0,0 +1,18 @@
+﻿using restaurant.server.DTOs;
+using restaurant.server.Repositories;
+
+namespace restaurant.server.Services;
+
+public class StaffService(StaffRepository staffRepository)
+{
+    public async Task<List<StaffModel>> GetAll()
+    {
+        return await staffRepository.GetAll();
+    }
+
+    public async Task<StaffModel?> GetById(int id)
+    {
+        var staffModel = await staffRepository.GetById(id);
+        return staffModel ?? null;
+    }
+}
