c24672393de5125a0e44b3147e51752d9ecf67df
Alexey Shatskikh <shtskkh@gmail.com>
2025-04-21 12:58:41 +0700

    DishesService: GetAll, GetById

---
+++ restaurant.server/Services/DishesService.cs
@@ -0,0 +1,31 @@
+﻿using restaurant.server.DTOs;
+using restaurant.server.Repositories;
+
+namespace restaurant.server.Services;
+
+public interface IDishesService
+{
+    Task<List<DishModel>> GetAll();
+    Task<DishModel?> GetById(int id);
+}
+
+public class DishesService(IDishesRepository dishesRepository) : IDishesService
+{
+    public async Task<List<DishModel>> GetAll()
+    {
+        return await dishesRepository.GetAll();
+    }
+
+    public async Task<DishModel?> GetById(int id)
+    {
+        var dishModel = await dishesRepository.GetById(id);
+
+        if (dishModel == null)
+            return null;
+
+        var products = await dishesRepository.GetProductsInDish(dishModel.IdDish);
+        dishModel.Products = products;
+
+        return dishModel;
+    }
+}
