a2a5ca172cdb44c8ef23b168f2e1297d810f3fdd
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-01 11:55:39 +0700

    JwtService: GenerateJwtToken

---
+++ restaurant.server/Services/JwtService.cs
@@ -0,0 +1,31 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using restaurant.server.DTOs;
+
+namespace restaurant.server.Services;
+
+public interface IJwtService
+{
+    string GenerateJwtToken();
+}
+
+public class JwtService(IOptions<JwtSettingsModel> jwtSettings) : IJwtService
+{
+    private readonly JwtSettingsModel _jwtSettings = jwtSettings.Value;
+
+    public string GenerateJwtToken()
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var jwt = new JwtSecurityToken(
+            _jwtSettings.Issuer,
+            _jwtSettings.Audience,
+            expires: DateTime.Now.AddMinutes(_jwtSettings.AccessTokenExpirationMinutes),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
+}
