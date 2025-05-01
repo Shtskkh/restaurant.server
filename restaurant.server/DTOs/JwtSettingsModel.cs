bbecb2435ca9a66d2fb55661e76910da70ea7d35
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-01 11:32:22 +0700

    Добавлена модель для настроек jwt

---
+++ restaurant.server/DTOs/JwtSettingsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace restaurant.server.DTOs;
+
+public class JwtSettingsModel
+{
+    public required string SecretKey { get; set; }
+    public required string Issuer { get; set; }
+    public required string Audience { get; set; }
+    public required int AccessTokenExpirationMinutes { get; set; }
+}
