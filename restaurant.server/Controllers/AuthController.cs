adc06435f5233dc5931484f3c61cdb56452a81af
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-01 12:22:09 +0700

    AuthController: Authenticate

---
+++ restaurant.server/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restaurant.server.DTOs;
+using restaurant.server.Services;
+
+namespace restaurant.server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class AuthController(IAuthService authService) : Controller
+{
+    [HttpPost("login")]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Authenticate([FromForm] AuthRequestModel request)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        
+        var token = await authService.Authenticate(request.Login, request.Password);
+        if (string.IsNullOrEmpty(token))
+            return NotFound(new ProblemDetails
+            {
+                Detail = "Неверный логин или пароль.",
+                Status = 404
+            });
+        
+        return Ok(new { access_token = token });
+    }
+}
