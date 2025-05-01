fe586dc894f19954c751e4c9f00525d02ffca27b
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-01 12:05:51 +0700

    AuthService: Authenticate

---
+++ restaurant.server/Services/AuthService.cs
@@ -0,0 +1,20 @@
+﻿using restaurant.server.Repositories;
+
+namespace restaurant.server.Services;
+
+public interface IAuthService
+{
+    Task<string?> Authenticate(string login, string password);
+}
+
+public class AuthService(IStaffRepository staffRepository, IJwtService jwtService) : IAuthService
+{
+    public async Task<string?> Authenticate(string login, string password)
+    {
+        var staff = await staffRepository.GetLoginInfo(login);
+        if (staff == null || staff.Password != password)
+            return null;
+
+        return jwtService.GenerateJwtToken();
+    }
+}
