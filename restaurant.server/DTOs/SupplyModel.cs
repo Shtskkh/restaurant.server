7c5f8992c669bdfc80e7f10d7b8c839a4f508d0e
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-02 20:40:43 +0700

    DTOs: добавлена модель для поставок

---
+++ restaurant.server/DTOs/SupplyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace restaurant.server.DTOs;
+
+public class SupplyModel
+{
+    public required string Product { get; set; }
+    public required decimal Count { get; set; }
+    public required DateTime Date { get; set; }
+    public required string Unit { get; set; }
+    public required string Supplier { get; set; }
+}
