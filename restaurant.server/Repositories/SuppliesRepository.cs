a740930bb88d8163c54e482cefbc3c3083d472ea
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-02 20:40:59 +0700

    SuppliesRepository: GetAll

---
+++ restaurant.server/Repositories/SuppliesRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using restaurant.server.Context;
+using restaurant.server.DTOs;
+
+namespace restaurant.server.Repositories;
+
+public interface ISuppliesRepository
+{
+    Task<List<SupplyModel>> GetAll();
+}
+
+public class SuppliesRepository(RestaurantContext context) : ISuppliesRepository
+{
+    public async Task<List<SupplyModel>> GetAll()
+    {
+        var suppliesModels =
+            from supply in context.Supplies.AsNoTracking()
+            join supplier in context.Suppliers.AsNoTracking()
+                on supply.IdSupplier equals supplier.IdSupplier
+            join product in context.Products.AsNoTracking()
+                on supply.IdProduct equals product.IdProduct
+            join unit in context.MeasureUnits.AsNoTracking()
+                on supply.IdUnit equals unit.IdUnit
+            select new SupplyModel
+            {
+                Product = product.Title,
+                Count = supply.Count,
+                Date = supply.Date,
+                Unit = unit.Title,
+                Supplier = supplier.Title
+            };
+
+        return await suppliesModels.ToListAsync();
+    }
+}
