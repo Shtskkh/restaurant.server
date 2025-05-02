e19056a52ba2d815ee21cd1fea5e03e65e5be8b5
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-02 20:44:53 +0700

    SuppliesService: GetAll

---
+++ restaurant.server/Services/SuppliesService.cs
@@ -0,0 +1,17 @@
+﻿using restaurant.server.DTOs;
+using restaurant.server.Repositories;
+
+namespace restaurant.server.Services;
+
+public interface ISuppliesService
+{
+    Task<List<SupplyModel>> GetAll();
+}
+
+public class SuppliesService(ISuppliesRepository suppliesRepository) : ISuppliesService
+{
+    public async Task<List<SupplyModel>> GetAll()
+    {
+        return await suppliesRepository.GetAll();
+    }
+}
