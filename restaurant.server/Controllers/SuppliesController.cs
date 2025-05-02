c782d1a7faaf398ec7030de5e029828036e0b76f
Alexey Shatskikh <shtskkh@gmail.com>
2025-05-02 20:48:46 +0700

    SuppliesController: GetAll

---
+++ restaurant.server/Controllers/SuppliesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restaurant.server.DTOs;
+using restaurant.server.Services;
+
+namespace restaurant.server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class SuppliesController(ISuppliesService suppliesService) : Controller
+{
+    [HttpGet("GetAll")]
+    [ProducesResponseType(typeof(List<SupplyModel>), 200)]
+    [ProducesResponseType(204)]
+    public async Task<IActionResult> GetAll()
+    {
+        var supplies = await suppliesService.GetAll();
+        if (supplies.Count == 0)
+            return NoContent();
+
+        return Ok(supplies);
+    }
+}
